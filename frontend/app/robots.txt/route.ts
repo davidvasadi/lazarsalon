6f24987250d7aa4eb36d998817242bea6fafba5a
Dave <david.vasadi@icloud.com>
2026-02-13 06:10:11 +0100

    analytics added

--- frontend/app/robots.txt/route.ts
+++ frontend/app/robots.txt/route.ts
@@ -1,7 +1,7 @@
 // app/robots.txt/route.ts
 import { NextResponse } from 'next/server';
 
-export const revalidate = 60 * 60 * 12; // 12 óra
+export const revalidate = 600; // 12 óra
 
 function stripSlash(u: string) {
   return u.replace(/\/$/, '');
